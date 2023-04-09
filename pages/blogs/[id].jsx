f43aaeb6ed6a0aad6b88e0030aaf7857f31e0a13
Mhd Ali Houria <alihoria123456@gmail.com>
2023-04-09 15:44:50 +0300

    Making the single blog page

--- pages/blogs/[id].jsx
+++ pages/blogs/[id].jsx
@@ -1,9 +1,101 @@
+import { Box, Card, styled } from "@mui/material";
+import Image from "next/image";
 import allBlogs from "../../src/utils/__api__/blogsList";
-import getBlogPost from "../../src/utils/__api__/blogSinglePost";
+// import getBlogPost from "../../src/utils/__api__/blogSinglePost";
 
-const SingleBlogPage = ({ id, postData }) => {
-  console.log(postData);
-  return <div>{id}</div>;
+const Wrapper = styled(Card)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  margin: "1.5rem",
+  padding: "1rem",
+  "@media only screen and (max-width: 625px)": {
+    flexWrap: "wrap",
+    img: {
+      height: "auto",
+      minWidth: "100%",
+    },
+  },
+}));
+
+const BlogTitle = styled(Box)({
+  fontSize: "1.5rem",
+  margin: ".5em 0 1em ",
+});
+
+
+const BlogPostMetaData = styled(Box)({
+  display: "flex",
+  flexWrap: "wrap",
+  margin: "1em 0",
+  "& span": {
+    padding: "0 .8rem 0 0 ",
+  },
+  "& span i": {
+    paddingRight: ".25rem",
+  },
+});
+
+const BlogDesc = styled(Box)(({ theme }) => ({
+  marginTop: "1.7rem",
+  fontSize: "1rem",
+  "@media only screen and (max-width: 625px)": {
+    fontSize: ".8rem"
+  },
+}));
+// =========================================================
+
+// =========================================================
+
+const SingleBlogPage = ({ urlId, postData }) => {
+  const {
+    id,
+    category_id,
+    category_name,
+    slug,
+    title,
+    description,
+    meta_keywords,
+    author_name,
+    view_count,
+    image,
+    created_date,
+  } = postData;
+  return (
+    <Wrapper>
+      <BlogTitle>{title}</BlogTitle>
+      <Image
+          src={`https://sinbad-store.com${image}`}
+          height={"400"}
+          width={"100%"}
+          objectFit="cover"
+        />
+      <BlogPostMetaData>
+        {author_name && (
+          <span>
+            <i className="fa-solid fa-user"></i> {author_name}
+          </span>
+        )}
+        {category_name && (
+          <span>
+            <i className="fa-solid fa-folder"></i> {category_name}
+          </span>
+        )}
+        {view_count && (
+          <span>
+            <i className="fa-solid fa-eye"></i>
+            {view_count}
+          </span>
+        )}
+        {created_date && (
+          <span>
+            <i className="fa-regular fa-calendar-days"></i>
+            {created_date}
+          </span>
+        )}
+      </BlogPostMetaData>
+      <BlogDesc>{description.replace(/(<([^>]+)>)/gi, "")}</BlogDesc>
+    </Wrapper>
+  );
 };
 
 export const getStaticPaths = (ctx) => {
@@ -33,9 +125,9 @@ export const getStaticProps = async (ctx) => {
   // Fetch Headers
   // -----------------------------------
 
-  const currentId = ctx.params.id;
+  const urlId = ctx.params.id;
   const postObj = await fetch(
-    `http://sinbad-store.com/api/v2/blog/${currentId}`,
+    `http://sinbad-store.com/api/v2/blog/${urlId}`,
     requestOptions
   )
     .then((res) => res.json())
@@ -43,7 +135,7 @@ export const getStaticProps = async (ctx) => {
 
   return {
     props: {
-      id: currentId,
+      urlId,
       postData: postObj.data,
     },
   };
