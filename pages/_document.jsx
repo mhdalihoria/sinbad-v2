c00fa0a30c83cf5e2692082afe874b4c880cde3d
Mhd Ali Houria <alihoria123456@gmail.com>
2023-04-12 14:28:21 +0300

    Adding New Font

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -26,6 +26,12 @@ export default class Bazaar extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
+                  <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
         </Head>
 
         <body>
