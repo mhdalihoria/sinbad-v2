e21c7f7e060472f9142e2ed08de01ea68d97eb4c
Mhd Ali Houria <alihoria123456@gmail.com>
2023-04-18 15:38:56 +0300

    Setting up Foundations for /files

--- pages/files/index.jsx
+++ pages/files/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Box, styled } from "@mui/material";
+import allFiles from "../../src/utils/__api__/files";
+import EmptyFiles from "../../src/pages-sections/files/EmptyFiles";
 
-const Files = () => {
+const PageContainer = styled(Box)({
+  border: "1px solid #eaeaea",
+  width: "80%",
+  margin: "2rem auto",
+  padding: "2rem",
+})
+
+const Files = ({ allFiles }) => {
+  const {data} = allFiles.files
   return (
-    <div>Files</div>
-  )
-}
+    <PageContainer>
+    {data.length === 0 ? <EmptyFiles />: <p>something</p>}
+    </PageContainer>
+  );
+};
 
-export default Files
+export const getStaticProps = async (ctx) => {
+  const everyFile = await allFiles;
+
+  return {
+    props: {
+      allFiles: everyFile.data,
+    },
+  };
+};
+
+export default Files;
---
+++ src/pages-sections/files/EmptyFiles.jsx
@@ -0,0 +1,14 @@
+const EmptyFilesDivStyle={
+    fontWeight: "700",
+    fontSize: "1.5rem",
+    textAlign: "center"
+
+}
+
+const EmptyFiles = () => {
+  return (
+    <div style={EmptyFilesDivStyle}>لا يوجد ملفات في الوقت الحالي</div>
+  )
+}
+
+export default EmptyFiles
