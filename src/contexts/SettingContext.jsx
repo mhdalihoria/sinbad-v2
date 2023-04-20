46a75b23b00a8a97cef4d50de34d0fc636942099
Mhd Ali Houria <alihoria123456@gmail.com>
2023-04-20 17:59:11 +0300

    Adding Site Settings logic in Context File

--- src/contexts/SettingContext.jsx
+++ src/contexts/SettingContext.jsx
@@ -7,35 +7,88 @@ import { createContext, useEffect, useState } from "react";
 // SET "rtl" OR "ltr" HERE
 // THEN GOTO BROWSER CONSOLE AND RUN localStorage.clear() TO CLEAR LOCALSTORAGE
 const initialSettings = {
-  direction: "rtl"
+  direction: "rtl",
 };
 export const SettingsContext = createContext({
   settings: initialSettings,
-  updateSettings: arg => {}
+  siteSettingsData: {},
+  updateSettings: (arg) => {},
 });
 
 // ============================================================
 
 // ============================================================
 
-const SettingsProvider = ({
-  children
-}) => {
+const SettingsProvider = ({ children }) => {
   const [settings, setSettings] = useState(initialSettings);
-  const updateSettings = updatedSetting => {
+  const [siteSettingsData, setSiteSettingsData] = useState({});
+  const [fetchSiteSettings, setFetchSiteSettings] = useState();
+  const updateSettings = (updatedSetting) => {
     setSettings(updatedSetting);
-    window.localStorage.setItem("bazaar_settings", JSON.stringify(updatedSetting));
+    window.localStorage.setItem(
+      "bazaar_settings",
+      JSON.stringify(updatedSetting)
+    );
   };
+
+  // UseEffect to Get Data from API and assigning it to seperate State
+  useEffect(() => {
+    const getAllSettings = async () => {
+      // -----------REQUEST HEADER---------
+      const myHeaders = new Headers();
+      myHeaders.append("X-localization", "ar");
+      // -----------REQUEST HEADER ---------
+
+      // -----------REQUEST OPTIONS---------
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+      // -----------REQUEST OPTIONS---------
+
+      try {
+        const response = await fetch(
+          "https://sinbad-store.com/api/v2/settings",
+          requestOptions
+        );
+        const data = await response.json();
+        setFetchSiteSettings(data.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getAllSettings();
+  }, []);
+  // UseEffect to Get Data from LocalStorage and Assigning it to State
   useEffect(() => {
     if (!window) return null;
+    // -----------------------------------------------
     const getItem = window.localStorage.getItem("bazaar_settings");
     if (getItem) setSettings(JSON.parse(getItem));
-  }, []);
-  return <SettingsContext.Provider value={{
-    settings,
-    updateSettings
-  }}>
+    // -----------------------------------------------
+    const getSiteSettings = window.localStorage.getItem("siteData_settings");
+    // if it exists assign it to state (siteSettings)
+    if (getSiteSettings && getSiteSettings !== "undefined"){
+      setSiteSettingsData(JSON.parse(getSiteSettings));
+    }
+    // if it doesn't, get the info from the fetch state and assign the local storage and the settings state to the fetched state
+    if(!getSiteSettings || getSiteSettings === "undefined" || getSiteSettings === {}) {
+      window.localStorage.setItem("siteData_settings", JSON.stringify(fetchSiteSettings));
+      setSiteSettingsData(fetchSiteSettings);
+    }
+  }, [fetchSiteSettings]);
+
+  return (
+    <SettingsContext.Provider
+      value={{
+        settings,
+        updateSettings,
+        siteSettingsData,
+      }}
+    >
       {children}
-    </SettingsContext.Provider>;
+    </SettingsContext.Provider>
+  );
 };
 export default SettingsProvider;
