7999d412a8658f6f23c9de5084b4d7a7a8cbe852
Mhd Ali Houria <alihoria123456@gmail.com>
2023-04-20 17:58:10 +0300

    Fixing Small Warning

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -27,7 +27,7 @@ export default class Bazaar extends Document {
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
                   <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;700&display=swap"
           rel="stylesheet"
