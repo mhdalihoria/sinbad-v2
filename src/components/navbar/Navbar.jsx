53a618860f6a826971da9f81ce43c87a2592737f
Mhd Ali Houria <alihoria123456@gmail.com>
2023-04-26 13:17:32 +0300

    Adding Main Menu items to the NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,19 @@
-import { Box, Button, Container, MenuItem, styled } from "@mui/material";
+import { useContext } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  MenuItem,
+  styled,
+  useTheme,
+} from "@mui/material";
 import ArrowRight from "@mui/icons-material/ArrowRight";
-import { ArrowLeft, ChevronLeft, ChevronRight, KeyboardArrowDown } from "@mui/icons-material";
+import {
+  ArrowLeft,
+  ChevronLeft,
+  ChevronRight,
+  KeyboardArrowDown,
+} from "@mui/icons-material";
 import { NavLink } from "components/nav-link";
 import { FlexBox } from "components/flex-box";
 import BazaarCard from "components/BazaarCard";
@@ -11,6 +24,7 @@ import MegaMenu from "./MegaMenu";
 import MegaMenu2 from "./MegaMenu2";
 import useSettings from "hooks/useSettings";
 import navbarNavigations from "data/navbarNavigations";
+import { SettingsContext } from "contexts/SettingContext";
 
 // NavList props interface
 
@@ -19,29 +33,25 @@ const navLinkStyle = {
   cursor: "pointer",
   transition: "color 150ms ease-in-out",
   "&:hover": {
-    color: "primary.main"
+    color: "primary.main",
   },
   "&:last-child": {
-    marginRight: 0
-  }
+    marginRight: 0,
+  },
 };
 // style components
 const StyledNavLink = styled(NavLink)({
-  ...navLinkStyle
+  ...navLinkStyle,
 });
-const ParentNav = styled(Box)(({
-  theme
-}) => ({
+const ParentNav = styled(Box)(({ theme }) => ({
   "&:hover": {
     color: theme.palette.primary.main,
     "& > .parent-nav-item": {
-      display: "block"
-    }
-  }
+      display: "block",
+    },
+  },
 }));
-const ParentNavItem = styled(Box)(({
-  theme
-}) => ({
+const ParentNavItem = styled(Box)(({ theme }) => ({
   top: 0,
   zIndex: 5,
   left: "100%",
@@ -51,37 +61,32 @@ const ParentNavItem = styled(Box)(({
   [theme.breakpoints.down(1640)]: {
     right: "100%",
     left: "auto",
-    paddingRight: 8
-  }
+    paddingRight: 8,
+  },
 }));
-const NavBarWrapper = styled(BazaarCard)(({
-  theme,
-  border
-}) => ({
+const NavBarWrapper = styled(BazaarCard)(({ theme, border }) => ({
   height: "60px",
   display: "block",
   borderRadius: "0px",
   position: "relative",
   boxShadow: "none",
   ...(border && {
-    borderBottom: `1px solid ${theme.palette.grey[200]}`
+    borderBottom: `1px solid ${theme.palette.grey[200]}`,
   }),
   [theme.breakpoints.down(1150)]: {
-    display: "none"
-  }
+    display: "none",
+  },
 }));
 const InnerContainer = styled(Container)({
   height: "100%",
   display: "flex",
   alignItems: "center",
-  justifyContent: "space-between"
+  justifyContent: "space-between",
 });
-const CategoryMenuButton = styled(Button)(({
-  theme
-}) => ({
+const CategoryMenuButton = styled(Button)(({ theme }) => ({
   width: "278px",
   height: "40px",
-  backgroundColor: theme.palette.grey[100]
+  backgroundColor: theme.palette.grey[100],
 }));
 const ChildNavsWrapper = styled(Box)({
   zIndex: 5,
@@ -89,129 +94,175 @@ const ChildNavsWrapper = styled(Box)({
   top: "100%",
   display: "none",
   position: "absolute",
-  transform: "translate(-50%, 0%)"
+  transform: "translate(-50%, 0%)",
 });
 
 // ==========================================================
 
 // ==========================================================
 
-const Navbar = ({
-  navListOpen,
-  hideCategories,
-  elevation,
-  border
-}) => {
-  const {
-    settings
-  } = useSettings();
+const Navbar = ({ navListOpen, hideCategories, elevation, border }) => {
+  const { settings } = useSettings();
+  const theme = useTheme();
+  const { siteSettingsData } = useContext(SettingsContext);
   const renderNestedNav = (list = [], isRoot = false) => {
-    return list.map(nav => {
+    return list.map((nav) => {
       if (isRoot) {
         // show megamenu
-        if (nav.megaMenu) {
-          return (
-            //@ts-ignore
-            <MegaMenu key={nav.title} title={nav.title} menuList={nav.child} />
-          );
-        }
+        // if (nav.megaMenu) {
+        //   return (
+        //     //@ts-ignore
+        //     <MegaMenu key={nav.title} title={nav.title} menuList={nav.child} />
+        //   );
+        // }
 
         // show megamenu with sub
-        if (nav.megaMenuWithSub) {
+        // if (nav.megaMenuWithSub) {
+        //   return (
+        //     //@ts-ignore
+        //     <MegaMenu2 key={nav.title} title={nav.title} menuList={nav.child} />
+        //   );
+        // }
+        if (nav.link && !nav.sub_items.length) {
           return (
-            //@ts-ignore
-            <MegaMenu2 key={nav.title} title={nav.title} menuList={nav.child} />
+            <StyledNavLink href={nav.link} key={nav.title}>
+              {nav.title}
+            </StyledNavLink>
           );
         }
-        if (nav.url) {
-          return <StyledNavLink href={nav.url} key={nav.title}>
-              {nav.title}
-            </StyledNavLink>;
-        }
-        if (nav.child) {
-          return <FlexBox key={nav.title} alignItems="center" position="relative" flexDirection="column" sx={{
-            "&:hover": {
-              "& > .child-nav-item": {
-                display: "block"
-              }
-            }
-          }}>
+        if (nav.sub_items.length) {
+          return (
+            <FlexBox
+              key={nav.title}
+              alignItems="center"
+              position="relative"
+              flexDirection="column"
+              sx={{
+                "&:hover": {
+                  "& > .child-nav-item": {
+                    display: "block",
+                  },
+                },
+              }}
+            >
               <FlexBox alignItems="flex-end" gap={0.3} sx={navLinkStyle}>
                 {nav.title}{" "}
-                <KeyboardArrowDown sx={{
-                color: "grey.500",
-                fontSize: "1.1rem"
-              }} />
+                <KeyboardArrowDown
+                  sx={{
+                    color: "grey.500",
+                    fontSize: "1.1rem",
+                  }}
+                />
               </FlexBox>
 
               <ChildNavsWrapper className="child-nav-item">
-                <BazaarCard elevation={3} sx={{
-                mt: 2.5,
-                py: 1,
-                minWidth: 200
-              }}>
-                  {renderNestedNav(nav.child)}
+                <BazaarCard
+                  elevation={3}
+                  sx={{
+                    mt: 2.5,
+                    py: 1,
+                    minWidth: 200,
+                  }}
+                >
+                  {renderNestedNav(nav.sub_items)}
                 </BazaarCard>
               </ChildNavsWrapper>
-            </FlexBox>;
+            </FlexBox>
+          );
         }
       } else {
-        if (nav.url) {
-          return <NavLink href={nav.url} key={nav.title}>
-              <MenuItem>{nav.title}</MenuItem>
-            </NavLink>;
+        if (nav.link) {
+          return (
+            <NavLink href={nav.link} key={nav.title}>
+              <MenuItem style={{ color: theme.palette.secondary[500] }}>
+                {nav.title}
+              </MenuItem>
+            </NavLink>
+          );
         }
-        if (nav.child) {
-          return <ParentNav position="relative" minWidth="230px" key={nav.title}>
+        if (nav.sub_items) {
+          return (
+            <ParentNav position="relative" minWidth="230px" key={nav.title}>
               <MenuItem color="grey.700">
                 <Box flex="1 1 0" component="span">
                   {nav.title}
                 </Box>
 
-                {settings.direction === "ltr" ? <ArrowRight fontSize="small" /> : <ArrowLeft fontSize="small" />}
+                {settings.direction === "ltr" ? (
+                  <ArrowRight fontSize="small" />
+                ) : (
+                  <ArrowLeft fontSize="small" />
+                )}
               </MenuItem>
 
               <ParentNavItem className="parent-nav-item">
-                <BazaarCard sx={{
-                py: "0.5rem",
-                minWidth: "230px"
-              }} elevation={3}>
-                  {renderNestedNav(nav.child)}
+                <BazaarCard
+                  sx={{
+                    py: "0.5rem",
+                    minWidth: "230px",
+                  }}
+                  elevation={3}
+                >
+                  {renderNestedNav(nav.sub_items)}
                 </BazaarCard>
               </ParentNavItem>
-            </ParentNav>;
+            </ParentNav>
+          );
         }
       }
     });
   };
-  return <NavBarWrapper hoverEffect={false} elevation={elevation} border={border}>
-      {!hideCategories ? <InnerContainer>
+
+  return (
+    <NavBarWrapper hoverEffect={false} elevation={elevation} border={border}>
+      {!hideCategories ? (
+        <InnerContainer>
           {/* Category megamenu */}
           <CategoryMenu open={navListOpen}>
             <CategoryMenuButton variant="text">
               <Category fontSize="small" />
-              <Paragraph fontWeight="600" textAlign="left" flex="1 1 0" ml={1.25} color="grey.600">
+              <Paragraph
+                fontWeight="600"
+                textAlign="left"
+                flex="1 1 0"
+                ml={1.25}
+                color="grey.600"
+              >
                 Categories
               </Paragraph>
 
-              {settings.direction === "ltr" ? <ChevronRight className="dropdown-icon" fontSize="small" /> : <ChevronLeft className="dropdown-icon" fontSize="small" />}
+              {settings.direction === "ltr" ? (
+                <ChevronRight className="dropdown-icon" fontSize="small" />
+              ) : (
+                <ChevronLeft className="dropdown-icon" fontSize="small" />
+              )}
             </CategoryMenuButton>
           </CategoryMenu>
 
           {/* Horizontal menu */}
-          <FlexBox gap={4}>{renderNestedNav(navbarNavigations, true)}</FlexBox>
-        </InnerContainer> : <InnerContainer sx={{
-      justifyContent: "center"
-    }}>
-          <FlexBox gap={4}>{renderNestedNav(navbarNavigations, true)}</FlexBox>
-        </InnerContainer>}
-    </NavBarWrapper>;
+          <FlexBox gap={4}>
+            {renderNestedNav(siteSettingsData.main_menu, true)}
+          </FlexBox>
+        </InnerContainer>
+      ) : (
+        <InnerContainer
+          sx={{
+            justifyContent: "center",
+          }}
+        >
+          <FlexBox gap={4}>
+            {renderNestedNav(siteSettingsData.main_menu, true)}
+          </FlexBox>
+        </InnerContainer>
+      )}
+    </NavBarWrapper>
+  );
 };
 
 //  set default props data
 Navbar.defaultProps = {
   elevation: 2,
   navListOpen: false,
-  hideCategories: false
+  hideCategories: false,
 };
 export default Navbar;
