1e8ed2c3ed347fbe38072397b2256633fd95fd6a
Mhd Ali Houria <alihoria123456@gmail.com>
2023-04-26 15:52:08 +0300

    Styling Confirm Code Page

--- src/pages-sections/sessions/ConfirmCode.jsx
+++ src/pages-sections/sessions/ConfirmCode.jsx
@@ -1,41 +1,80 @@
+import { Box, Button, Input, styled } from "@mui/material";
+import { H2 } from "components/Typography";
+
 const { default: usePostFetch } = require("components/fetch/usePostFetch");
 const { useState } = require("react");
 
+const HeaderStyle = styled(H2)({
+  textAlign: "center",
+  fontSize: "2rem",
+  marginBottom: "1rem",
+});
+const ErrorTxtStyle = styled(H2)(({theme}) => ({
+  textAlign: "center",
+  fontSize: "1rem",
+  marginBottom: "1rem",
+  color: theme.palette.primary.main
+}))
+
+const CodeConfirmationWrapper = styled(Box)({
+  height: "300px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+const InputStyle = styled(Input)({
+  padding: "10px 25px",
+  fontSize: "1rem",
+  letterSpacing: "1px",
+});
+const ButtonStyle = styled(Button)(({ theme }) => ({
+  padding: "10px 25px",
+  marginLeft: "1rem",
+  fontSize: "1rem",
+  background: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+}));
+
 const ConfirmCode = ({ token, setStage }) => {
-    const [input, setInput] = useState("");
-    const [error, setError] = useState("")
-    const handleChange = (event) => {
-      setInput(event.target.value);
-      console.log(input)
-    };
-    const handleSubmit = async () => {
-      const data = await usePostFetch(
-        "https://sinbad-store.com/api/v2/activate",
-        {
-          "X-localization": "ar",
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-        JSON.stringify({ code: input })
-      )
-      console.log(data)
-      if(data.success) setStage(2)
-      if(!data.success) setError(data.message)
-    }
-    return (
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+  const handleSubmit = async () => {
+    const data = await usePostFetch(
+      "https://sinbad-store.com/api/v2/activate",
+      {
+        "X-localization": "ar",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      JSON.stringify({ code: input })
+    );
+    console.log(data);
+    if (data.status) setStage(2);
+    if (!data.status) setError(data.message);
+  };
+
+  return (
+    <CodeConfirmationWrapper>
+      <HeaderStyle>Confirm Your Account</HeaderStyle>
+      {error !== "" && <ErrorTxtStyle>{error}</ErrorTxtStyle>}
       <div>
-        <h2>Confirm Your Account</h2>
-        {error !== "" && <h5 style={{color: "red"}}>{error}</h5>}
-        <input
+        <InputStyle
           type="text"
           placeholder="Activation code here"
           value={input}
           name={"input"}
           onChange={handleChange}
         />
-        <button onClick={handleSubmit}>Submit</button>
+        <ButtonStyle onClick={handleSubmit}>Submit</ButtonStyle>
       </div>
-    );
-  };
-
-  export default ConfirmCode
+      <span>Didn't get code? <span onClick={()=>console.log("code resent!")} style={{cursor: "pointer", textDecoration: "underline"}}>click here</span></span>
+    </CodeConfirmationWrapper>
+  );
+};
+9
+export default ConfirmCode;
