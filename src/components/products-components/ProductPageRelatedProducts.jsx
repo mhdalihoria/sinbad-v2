598b2b64aeb610f94ac34299db0394c4f20a22e7
Mhd Ali Houria <alihoria123456@gmail.com>
2023-05-06 16:51:48 +0300

    Making the Carousel Work Correctly

--- src/components/products-components/ProductPageRelatedProducts.jsx
+++ src/components/products-components/ProductPageRelatedProducts.jsx
@@ -1,14 +1,40 @@
+import { useTheme } from "@mui/material";
 import ProductCard from "./ProductCard";
-import React from "react";
+import { useState, useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
-import { Paper, Button } from "@mui/material";
 
-function ProductPageRelatedProducts({ relatedProducts }) {
-  console.log(chunkArray(relatedProducts, 3));
+const ProductPageRelatedProducts = ({ relatedProducts }) => {
+  const [windowSize, setWindowSize] = useState();
+  const [cardsPerPage, setCardsPerPage] = useState(2);
+  const theme = useTheme();
 
+  console.log(windowSize);
+  useEffect(() => {
+    const handleResizeWindow = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResizeWindow);
+    handleResizeWindow();
+
+    if (windowSize >= theme.breakpoints.values.md) {
+      setCardsPerPage(3);
+    } else if (
+      windowSize > theme.breakpoints.values.sm &&
+      windowSize < theme.breakpoints.values.md
+    ) {
+      setCardsPerPage(2);
+    } else {
+      setCardsPerPage(1);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, [windowSize]);
   return (
-    <Carousel navButtonsAlwaysVisible={true}>
-      {chunkArray(relatedProducts, 3).map((productChunk, idx) => {
+    <Carousel navButtonsAlwaysVisible={true} indicators={false}>
+      {chunkArray(relatedProducts, cardsPerPage).map((productChunk, idx) => {
         return (
           <div style={{ display: "flex" }} key={idx}>
             {productChunk.map((product, i) => {
@@ -19,7 +45,7 @@ function ProductPageRelatedProducts({ relatedProducts }) {
       })}
     </Carousel>
   );
-}
+};
 
 export default ProductPageRelatedProducts;
 
