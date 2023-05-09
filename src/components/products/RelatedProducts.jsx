66569a22c33fb92fb5244a35be299444b991b764
Mhd Ali Houria <alihoria123456@gmail.com>
2023-05-09 15:15:11 +0300

    Adding Carousel

--- src/components/products/RelatedProducts.jsx
+++ src/components/products/RelatedProducts.jsx
@@ -1,13 +1,24 @@
 import { Box, Grid } from "@mui/material";
+import Carousel from "components/carousel/Carousel";
 import ProductCard1 from "components/product-cards/ProductCard1";
 import { H3 } from "components/Typography";
+import useWindowSize from "hooks/useWindowSize";
+import { useEffect, useState } from "react";
+import { carouselStyled } from "components/carousel/styles";
+
 // ===================================================
 
 const RelatedProducts = ({ productsData }) => {
+  const width = useWindowSize();
+  const [visibleSlides, setVisibleSlides] = useState(4);
+  useEffect(() => {
+    if (width < 426) setVisibleSlides(1);else if (width < 650) setVisibleSlides(2);else if (width < 1024) setVisibleSlides(3);else if (width < 1200) setVisibleSlides(4);else setVisibleSlides(5);
+  }, [width]);
   return (
     <Box mb={7.5}>
       <H3 mb={3}>Realted Products</H3>
-      <Grid container spacing={8}>
+      <Carousel totalSlides={productsData.length} visibleSlides={visibleSlides} sx={carouselStyled}>
+      
         {productsData.map((item, ind) => (
           <Grid item lg={3} md={4} sm={6} xs={12} key={ind}>
             <ProductCard1
@@ -22,7 +33,7 @@ const RelatedProducts = ({ productsData }) => {
             />
           </Grid>
         ))}
-      </Grid>
+      </Carousel>
     </Box>
   );
 };
