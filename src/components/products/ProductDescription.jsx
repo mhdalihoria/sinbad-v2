78bc610cfd520c0c7a61a97c7f4c37860c57f961
Mhd Ali Houria <alihoria123456@gmail.com>
2023-05-09 16:09:49 +0300

    Changing Content to description rathere than "specs"

--- src/components/products/ProductDescription.jsx
+++ src/components/products/ProductDescription.jsx
@@ -8,27 +8,40 @@ import { H3 } from "components/Typography";
 const ProductDescription = ({ product }) => {
   return (
     <Box>
-      <H3 mb={2}>المواصفات:</H3>
+      <H3 mb={2}>الوصف:</H3>
       {typeof product !== "undefined" ? (
+        // <Box>
+        //   {product.shop_name.length > 1 && ( //display shop name if the name isn't an empty string
+        //     <span>
+        //       المتجر: {product.shop_name && product.shop_name}
+        //       {/* {product.shop_logo && product.shop_logo} */}
+        //     </span>
+        //   )}{" "}
+        //   <br />
+        //   {product.brand_name.length > 1 && ( //display shop name if the name isn't an empty string
+        //     <>
+        //       <span>
+        //         ماركة: {product.brand_name && product.brand_name}
+        //         {product.brand_logo && product.brand_logo}
+        //       </span>
+        //       <br />
+        //     </>
+        //   )}
+        //   <span> الحالة: {product.status}</span> <br />
+        //   <span>التصنيف: {product.category_name}</span> <br />
+        // </Box>
         <Box>
-          {product.shop_name.length > 1 && ( //display shop name if the name isn't an empty string
-            <span>
-              المتجر: {product.shop_name && product.shop_name}
-              {/* {product.shop_logo && product.shop_logo} */}
-            </span>
-          )}{" "}
-          <br />
-          {product.brand_name.length > 1 && ( //display shop name if the name isn't an empty string
-            <>
-              <span>
-                ماركة: {product.brand_name && product.brand_name}
-                {product.brand_logo && product.brand_logo}
-              </span>
-              <br />
-            </>
-          )}
-          <span> الحالة: {product.status}</span> <br />
-          <span>التصنيف: {product.category_name}</span> <br />
+          {product.product_description
+            .replace(/(<([^>]+)>)/gi, "")
+            .split(/&nbsp;/gi)
+            .map(function (item, idx) {
+              return (
+                <span key={idx}>
+                  {item}
+                  <br />
+                </span>
+              );
+            })}
         </Box>
       ) : (
         <span>Loading...</span>
