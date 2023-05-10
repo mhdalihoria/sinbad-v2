5c699e847d0e36f029a0f14baaf976e056d8e725
Mhd Ali Houria <alihoria123456@gmail.com>
2023-05-10 16:43:35 +0300

    Attempting to make localStorage work with cart

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,77 +1,66 @@
-import { createContext, useContext, useMemo, useReducer } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useReducer,
+} from "react";
 
 // =================================================================================
 
 // =================================================================================
 
-const INITIAL_CART = [{
-  qty: 1,
-  price: 210,
-  slug: "silver-high-neck-sweater",
-  name: "Silver High Neck Sweater",
-  id: "6e8f151b-277b-4465-97b6-547f6a72e5c9",
-  imgUrl: "/assets/images/products/Fashion/Clothes/1.SilverHighNeckSweater.png"
-}, {
-  qty: 1,
-  price: 110,
-  slug: "yellow-casual-sweater",
-  name: "Yellow Casual Sweater",
-  id: "76d14d65-21d0-4b41-8ee1-eef4c2232793",
-  imgUrl: "/assets/images/products/Fashion/Clothes/21.YellowCasualSweater.png"
-}, {
-  qty: 1,
-  price: 140,
-  slug: "denim-blue-jeans",
-  name: "Denim Blue Jeans",
-  id: "0fffb188-98d8-47f7-8189-254f06cad488",
-  imgUrl: "/assets/images/products/Fashion/Clothes/4.DenimBlueJeans.png"
-}];
+const INITIAL_CART = [ ];
 const INITIAL_STATE = {
-  cart: INITIAL_CART
+  cart: INITIAL_CART,
 };
 const AppContext = createContext({
   state: INITIAL_STATE,
-  dispatch: () => {}
+  dispatch: () => {},
 });
 const reducer = (state, action) => {
   switch (action.type) {
+    case "INITIAL_STORAGE":
+      return {...state};
     case "CHANGE_CART_AMOUNT":
       let cartList = state.cart;
       let cartItem = action.payload;
-      let exist = cartList.find(item => item.id === cartItem.id);
+      let exist = cartList.find((item) => item.id === cartItem.id);
       if (cartItem.qty < 1) {
-        const filteredCart = cartList.filter(item => item.id !== cartItem.id);
+        const filteredCart = cartList.filter((item) => item.id !== cartItem.id);
         return {
           ...state,
-          cart: filteredCart
+          cart: filteredCart,
         };
       }
 
       // IF PRODUCT ALREADY EXITS IN CART
       if (exist) {
-        const newCart = cartList.map(item => item.id === cartItem.id ? {
-          ...item,
-          qty: cartItem.qty
-        } : item);
+        const newCart = cartList.map((item) =>
+          item.id === cartItem.id
+            ? {
+                ...item,
+                qty: cartItem.qty,
+              }
+            : item
+        );
         return {
           ...state,
-          cart: newCart
+          cart: newCart,
         };
       }
       return {
         ...state,
-        cart: [...cartList, cartItem]
+        cart: [...cartList, cartItem],
       };
-    default:
-      {
-        return state;
-      }
+    default: {
+      return state;
+    }
   }
 };
 
-
 // This is how the dispatch function from useReducer was used (MiniCart.jsx)
-// dispatch({ 
+// dispatch({
 //   type: "CHANGE_CART_AMOUNT",
 //   payload: {
 //     ...product,
@@ -84,15 +73,44 @@ const reducer = (state, action) => {
 
 // =======================================================
 
-export const AppProvider = ({
-  children
-}) => {
+export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
-  const contextValue = useMemo(() => ({
-    state,
-    dispatch
-  }), [state, dispatch]);
-  return <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>;
+  const contextValue = useMemo(
+    () => ({
+      state,
+      dispatch,
+    }),
+    [state, dispatch]
+  );
+
+  useEffect(() => {
+    const itemsFromLocalStorage = JSON.parse(localStorage.getItem("cart"))
+    if (itemsFromLocalStorage) {
+      //checking if there already is a state in localstorage
+      console.log("it does exist in localStorage");
+      console.log(itemsFromLocalStorage.cart, state)
+      // itemsFromLocalStorage.cart => array of objects => [{}]
+      // state is also => array of objects => [{}]
+      //so if we want to put itemsFromLocalStorage.cart => we need to make it so it's only storing the objects inside the array of objects
+      dispatch({
+        type: "CHANGE_CART_AMOUNT",
+        payload: itemsFromLocalStorage.cart
+      })
+    }
+  }, []);
+
+  useEffect(() => {
+    if (state !== INITIAL_STATE) {
+      console.log("they ain't equqal");
+      window.localStorage.setItem("cart", JSON.stringify(state));
+      //create and/or set a new localstorage variable called "state"
+      console.log(state)
+    }
+  }, [state]);
+
+  return (
+    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+  );
 };
 export const useAppContext = () => useContext(AppContext);
 export default AppContext;
