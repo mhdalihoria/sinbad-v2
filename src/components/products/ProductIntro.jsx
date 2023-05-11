4bcbf23398081e66b43610954b0d253358ebf605
Mhd Ali Houria <alihoria123456@gmail.com>
2023-05-11 18:34:01 +0300

    Populating ProductIntro

--- src/components/products/ProductIntro.jsx
+++ src/components/products/ProductIntro.jsx
@@ -4,7 +4,7 @@ import { Add, Remove } from "@mui/icons-material";
 import { Avatar, Box, Button, Chip, Grid } from "@mui/material";
 import LazyImage from "components/LazyImage";
 import BazaarRating from "components/BazaarRating";
-import { H1, H2, H3, H6 } from "components/Typography";
+import { H1, H2, H3, H5, H6, Span } from "components/Typography";
 import { useAppContext } from "contexts/AppContext";
 import { FlexBox, FlexRowCenter } from "../flex-box";
 import { currency } from "lib";
@@ -25,6 +25,14 @@ const ProductIntro = ({ product, productImages }) => {
     shop_name,
     slug,
     thumbnail,
+    has_offer,
+    sale_price,
+    product_quantity,
+    display_quantity,
+    with_delivery_fee,
+    view_count,
+    brand_name,
+    brand_logo,
   } = product;
   const { state, dispatch } = useAppContext();
   const [selectedImage, setSelectedImage] = useState(0);
@@ -61,11 +69,20 @@ const ProductIntro = ({ product, productImages }) => {
       },
     });
   };
+
+  const displayAvailable = (productQuantity, displayQuantity) => {
+    if (!productQuantity || productQuantity === 0) {
+      return "Out Of Stock";
+    } else if (displayQuantity && productQuantity > 0) {
+      return `${productQuantity} قطعة`;
+    } else if (!displayQuantity && product_quantity === null) {
+      return "Available";
+    }
+  };
   return (
     <Box width="100%">
       <Grid container spacing={3} justifyContent="space-around">
-
-      <ProductImageViewer productImages={productImages} product={product} />
+        <ProductImageViewer productImages={productImages} product={product} />
         <Grid item md={6} xs={12} alignItems="center">
           <H1 mb={1}>{product_name}</H1>
 
@@ -110,15 +127,57 @@ const ProductIntro = ({ product, productImages }) => {
             </Box>
           ))} */}
 
-          <Box pt={1} mb={3}>
-            <H2 color="primary.main" mb={0.5} lineHeight="1">
-              {product_price}
-              {/* {currency(product_price)} */}
-            </H2>
-            <Box color="inherit">Stock Available</Box>
+          {with_delivery_fee !== false && (
+            <Box mb={1}>
+              <H5>بدون توصيل</H5>
+            </Box>
+          )}
+          <Box pt={1} mb={2}>
+            <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
+              {has_offer ? (
+                <>
+                  <H2 color="primary.main" mb={0.5} lineHeight="1">
+                    {sale_price}
+                    {/* {currency(product_price)} */}
+                  </H2>
+                  <del style={{ color: "grey" }}>{product_price}</del>
+                </>
+              ) : (
+                <H2 color="primary.main" mb={0.5} lineHeight="1">
+                  {product_price}
+                </H2>
+              )}
+            </div>
+            <Box color="inherit">
+              {displayAvailable(product_quantity, display_quantity)}
+            </Box>
           </Box>
+          {view_count && (
+            <Box mb={1}>
+              <Span>عدد المشاهدات: {view_count}</Span>
+            </Box>
+          )}
+          {(brand_name.length > 1 || brand_logo.length > 1) && (
+            <Box mb={1}>
+              <Span>
+                الماركة: {brand_name} {brand_logo}
+              </Span>
+            </Box>
+          )}
+          {shop_name.length > 1 && (
+            <FlexBox alignItems="center" mb={2}>
+              <Box>المتجر:</Box>
+              <Link href="/" passHref>
+                <a>
+                  <H6 ml={1}>{shop_name}</H6>
+                </a>
+              </Link>
+            </FlexBox>
+          )}
 
-         {product.is_global == 1 && <ProductPageImportedProduct product={product} />}
+          {product.is_global == 1 && (
+            <ProductPageImportedProduct product={product} />
+          )}
 
           {!cartItem?.qty ? (
             <Button
@@ -135,7 +194,7 @@ const ProductIntro = ({ product, productImages }) => {
               Add to Cart
             </Button>
           ) : (
-                <FlexBox alignItems="center" mb={4.5} mt={2.5}>
+            <FlexBox alignItems="center" mb={4.5} mt={2.5}>
               <Button
                 size="small"
                 sx={{
@@ -163,17 +222,6 @@ const ProductIntro = ({ product, productImages }) => {
               >
                 <Add fontSize="small" />
               </Button>
-            </FlexBox>
-          )}
-
-          {shop_name.length > 1 && (
-            <FlexBox alignItems="center" mb={2}>
-              <Box>المتجر:</Box>
-              <Link href="/" passHref>
-                <a>
-                  <H6 ml={1}>{shop_name}</H6>
-                </a>
-              </Link>
             </FlexBox>
           )}
         </Grid>
