77f4b082c76e55f09f5f1702ab60711d1e97a916
Mhd Ali Houria <alihoria123456@gmail.com>
2023-05-28 17:35:03 +0300

    Adding Logic to Get the Countries and Other geogragphic data

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -3,12 +3,13 @@ import SEO from "components/SEO";
 import CheckoutForm from "pages-sections/checkout/CheckoutForm";
 import CheckoutNavLayout from "components/layouts/CheckoutNavLayout";
 import CheckoutSummary from "pages-sections/checkout/CheckoutSummary";
-const Checkout = () => {
+import useGetFetch from "../src/components/fetch/useGetFetch";
+const Checkout = ({allCountries}) => {
   return <CheckoutNavLayout>
       <SEO title="Checkout" />
       <Grid container flexWrap="wrap-reverse" spacing={3}>
         <Grid item lg={8} md={8} xs={12}>
-          <CheckoutForm />
+          <CheckoutForm allCountries={allCountries}/>
         </Grid>
 
         <Grid item lg={4} md={4} xs={12}>
@@ -16,5 +17,19 @@ const Checkout = () => {
         </Grid>
       </Grid>
     </CheckoutNavLayout>;
+};
+export const getStaticProps = async () => {
+  const requestOptions= {
+    method: "GET",
+    headers: {"X-localization": "ar"},
+  }
+  const url = "https://sinbad-store.com/api/v2/countries"
+  const everyCountry = await useGetFetch(url, requestOptions)
+
+  return {
+    props: {
+      allCountries: everyCountry.data.countries,
+    },
+  };
 };
 export default Checkout;
