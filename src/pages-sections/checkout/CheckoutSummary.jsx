9b9d00bf5a7e5463fa9258664fc9f3d5e3ef379b
Mhd Ali Houria <alihoria123456@gmail.com>
2023-05-29 15:53:04 +0300

    Changing the shape of "CheckoutSummery.jsx"

--- src/pages-sections/checkout/CheckoutSummary.jsx
+++ src/pages-sections/checkout/CheckoutSummary.jsx
@@ -1,34 +1,41 @@
 import { Button, Divider, TextField, Typography } from "@mui/material";
 import Card1 from "components/Card1";
 import { FlexBetween } from "components/flex-box";
+import { useAppContext } from "contexts/AppContext";
 import { currency } from "lib";
 const CheckoutSummary = () => {
+  const {
+    state
+  } = useAppContext();
+  const cartList = state.cart;
+  const getTotalPrice = () => cartList.reduce((accum, item) => accum + item.price * item.qty, 0);
+  
   return <Card1>
       <FlexBetween mb={1}>
         <Typography color="grey.600">Subtotal:</Typography>
         <Typography fontSize="18px" fontWeight="600" lineHeight="1">
-          {currency(2610)}
+          {currency(getTotalPrice())}
         </Typography>
       </FlexBetween>
 
       <FlexBetween mb={1}>
         <Typography color="grey.600">Shipping:</Typography>
         <Typography fontSize="18px" fontWeight="600" lineHeight="1">
-          {currency(0)}
+          -
         </Typography>
       </FlexBetween>
 
       <FlexBetween mb={1}>
         <Typography color="grey.600">Tax:</Typography>
         <Typography fontSize="18px" fontWeight="600" lineHeight="1">
-          {currency(40)}
+          -
         </Typography>
       </FlexBetween>
 
       <FlexBetween mb={2}>
         <Typography color="grey.600">Discount:</Typography>
         <Typography fontSize="18px" fontWeight="600" lineHeight="1">
-          {currency(0)}
+          -
         </Typography>
       </FlexBetween>
 
@@ -37,16 +44,10 @@ const CheckoutSummary = () => {
     }} />
 
       <Typography fontSize="25px" fontWeight="600" lineHeight="1" textAlign="right" mb={3}>
-        {currency(2610)}
+        {currency(getTotalPrice())}
       </Typography>
 
-      <TextField placeholder="Voucher" variant="outlined" size="small" fullWidth />
-      <Button variant="outlined" color="primary" fullWidth sx={{
-      mt: "1rem",
-      mb: "30px"
-    }}>
-        Apply Voucher
-      </Button>
+      
     </Card1>;
 };
 export default CheckoutSummary;
