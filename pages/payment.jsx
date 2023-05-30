dbabc4eab5dc08180c3e55ce0a96069e2e11a229
Mhd Ali Houria <alihoria123456@gmail.com>
2023-05-30 18:40:37 +0300

    Getting List of Banks and giving it to the Payment Summery

--- pages/payment.jsx
+++ pages/payment.jsx
@@ -2,11 +2,12 @@ import { Grid } from "@mui/material";
 import PaymentForm from "pages-sections/payment/PaymentForm";
 import PaymentSummary from "pages-sections/payment/PaymentSummary";
 import CheckoutNavLayout from "components/layouts/CheckoutNavLayout";
-const Checkout = () => {
-  return <CheckoutNavLayout>
+import useGetFetch from "components/fetch/useGetFetch"
+const Checkout = ({banks}) => {
+   return <CheckoutNavLayout>
       <Grid container flexWrap="wrap-reverse" spacing={3}>
         <Grid item lg={8} md={8} xs={12}>
-          <PaymentForm />
+          <PaymentForm banks={banks}/>
         </Grid>
 
         <Grid item lg={4} md={4} xs={12}>
@@ -15,4 +16,20 @@ const Checkout = () => {
       </Grid>
     </CheckoutNavLayout>;
 };
+
+export const getStaticProps = async (ctx) => {
+  const requestOptions = {
+    method: 'GET',
+    headers: {"X-localization": "ar"},
+    redirect: 'follow'
+  };
+  
+  const response = await useGetFetch("https://sinbad-store.com/api/v2/get-banks",requestOptions)
+
+  return {
+    props: {
+      banks: response.data.banks
+    }
+  }
+}
 export default Checkout;
