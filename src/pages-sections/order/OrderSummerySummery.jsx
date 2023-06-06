0bfbf24e56e0ac5e1af918fa9861ff7dc043e30f
Mhd Ali Houria <alihoria123456@gmail.com>
2023-06-06 18:57:52 +0300

    Adding the Summery of Order Summery

---
+++ src/pages-sections/order/OrderSummerySummery.jsx
@@ -0,0 +1,46 @@
+import { Button, Divider, TextField, Typography } from "@mui/material";
+import Card1 from "components/Card1";
+import { FlexBetween } from "components/flex-box";
+import { useAppContext } from "contexts/AppContext";
+import { currency } from "lib";
+const OrderSummerySummery = () => {
+  const {
+    state
+  } = useAppContext();
+  const cartList = state.cart;
+  const getTotalPrice = () => cartList.reduce((accum, item) => accum + item.price * item.qty, 0);
+  
+  return <Card1>
+      <FlexBetween mb={1}>
+        <Typography color="grey.600">Subtotal:</Typography>
+        <Typography fontSize="18px" fontWeight="600" lineHeight="1">
+          {currency(getTotalPrice())}
+        </Typography>
+      </FlexBetween>
+
+      <FlexBetween mb={1}>
+        <Typography color="grey.600">Shipping:</Typography>
+        <Typography fontSize="18px" fontWeight="600" lineHeight="1">
+          -
+        </Typography>
+      </FlexBetween>
+
+      <FlexBetween mb={2}>
+        <Typography color="grey.600">Discount:</Typography>
+        <Typography fontSize="18px" fontWeight="600" lineHeight="1">
+          -
+        </Typography>
+      </FlexBetween>
+
+      <Divider sx={{
+      mb: "1rem"
+    }} />
+
+      <Typography fontSize="25px" fontWeight="600" lineHeight="1" textAlign="right" mb={3}>
+        {currency(getTotalPrice())}
+      </Typography>
+
+      
+    </Card1>;
+};
+export default OrderSummerySummery;
