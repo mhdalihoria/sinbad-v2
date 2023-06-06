fe0e1449565d7699d6c61e2f43b16b31d034c173
Mhd Ali Houria <alihoria123456@gmail.com>
2023-06-06 18:57:28 +0300

    Fixing Warning on Table

--- src/pages-sections/order/OrderSummeryTable.jsx
+++ src/pages-sections/order/OrderSummeryTable.jsx
@@ -18,155 +18,157 @@ const OrderSummeryTable = ({ data }) => {
   console.log(data);
   return (
     <Card1>
-      {data && (
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead style={{ background: theme.palette.primary.main }}>
-              <TableRow>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                  }}
-                  align="center"
-                >
-                  Price
-                </TableCell>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                  }}
-                  align="center"
-                >
-                  discount
-                </TableCell>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                  }}
-                  align="center"
-                >
-                  comission
-                </TableCell>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                  }}
-                  align="center"
-                >
-                  delivery comisison
-                </TableCell>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                  }}
-                  align="center"
-                >
-                  delivery fee?
-                </TableCell>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                  }}
-                  align="center"
-                >
-                  subtotal
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.cart_items.map((item, idx) => (
-                <TableRow
-                  key={idx}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell style={{ textAlign: "center" }}>
-                    {currency(item.price)}
+      <Paper>
+        {data && (
+          <TableContainer>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead style={{ background: theme.palette.primary.main }}>
+                <TableRow>
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                    }}
+                    align="center"
+                  >
+                    Price
                   </TableCell>
-                  <TableCell style={{ textAlign: "center" }}>
-                    {currency(item.discount)}
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                    }}
+                    align="center"
+                  >
+                    discount
                   </TableCell>
-                  <TableCell style={{ textAlign: "center" }}>
-                    {currency(item.commission)}
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                    }}
+                    align="center"
+                  >
+                    comission
                   </TableCell>
-                  <TableCell style={{ textAlign: "center" }}>
-                    {currency(item.delivery_commission)}
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                    }}
+                    align="center"
+                  >
+                    delivery comisison
                   </TableCell>
-                  <TableCell style={{ textAlign: "center" }}>
-                    with delivery fee? idk what to do here
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                    }}
+                    align="center"
+                  >
+                    delivery fee?
                   </TableCell>
-                  <TableCell style={{ textAlign: "center" }}>
-                    {currency(Number(item.qty) * item.price)} {`(qty*price)`}
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                    }}
+                    align="center"
+                  >
+                    subtotal
                   </TableCell>
-                  {/* // <TableCell scope="row" style={{ textAlign: "center" }}>
-                //   {item.delivery_within}
-                // </TableCell> */}
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead style={{ background: theme.palette.primary.main }}>
-              <TableRow>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                    textAlign: "center",
-                  }}
-                  align="center"
-                >
-                  Total Discount
-                </TableCell>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                    textAlign: "center",
-                  }}
-                  align="center"
-                >
-                  Shipping Cost
-                </TableCell>
-                <TableCell
-                  style={{
-                    color: theme.palette.primary.contrastText,
-                    fontWeight: "700",
-                    textAlign: "center",
-                  }}
-                  align="center"
-                >
-                  Total Price
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableCell style={{ textAlign: "center" }}>
-                {currency(data.total_discount)}
-              </TableCell>
-              <TableCell style={{ textAlign: "center" }}>
-                {currency(data.shipping_cost)}
-              </TableCell>
-              <TableCell style={{ textAlign: "center" }}>
-                {currency(
-                  data.cart_items.reduce(
-                    (acc, current) =>
-                      acc + Number(current.qty) * Number(current.price),
-                    0
-                  )
-                )}{" "}
-                <br />
-                {"qty*price for all items combined"}
-              </TableCell>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
+              </TableHead>
+              <TableBody>
+                {data.cart_items.map((item, idx) => (
+                  <TableRow
+                    key={idx}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell style={{ textAlign: "center" }}>
+                      {currency(item.price)}
+                    </TableCell>
+                    <TableCell style={{ textAlign: "center" }}>
+                      {currency(item.discount)}
+                    </TableCell>
+                    <TableCell style={{ textAlign: "center" }}>
+                      {currency(item.commission)}
+                    </TableCell>
+                    <TableCell style={{ textAlign: "center" }}>
+                      {currency(item.delivery_commission)}
+                    </TableCell>
+                    <TableCell style={{ textAlign: "center" }}>
+                      with delivery fee? idk what to do here
+                    </TableCell>
+                    <TableCell style={{ textAlign: "center" }}>
+                      {currency(Number(item.qty) * item.price)} {`(qty*price)`}
+                    </TableCell>
+                    {/* // <TableCell scope="row" style={{ textAlign: "center" }}>
+              //   {item.delivery_within}
+              // </TableCell> */}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead style={{ background: theme.palette.primary.main }}>
+                <TableRow>
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                      textAlign: "center",
+                    }}
+                    align="center"
+                  >
+                    Total Discount
+                  </TableCell>
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                      textAlign: "center",
+                    }}
+                    align="center"
+                  >
+                    Shipping Cost
+                  </TableCell>
+                  <TableCell
+                    style={{
+                      color: theme.palette.primary.contrastText,
+                      fontWeight: "700",
+                      textAlign: "center",
+                    }}
+                    align="center"
+                  >
+                    Total Price
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell style={{ textAlign: "center" }}>
+                    {currency(data.total_discount)}
+                  </TableCell>
+                  <TableCell style={{ textAlign: "center" }}>
+                    {currency(data.shipping_cost)}
+                  </TableCell>
+                  <TableCell style={{ textAlign: "center" }}>
+                    {currency(
+                      data.cart_items.reduce(
+                        (acc, current) =>
+                          acc + Number(current.qty) * Number(current.price),
+                        0
+                      )
+                    )}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </Paper>
     </Card1>
   );
 };
