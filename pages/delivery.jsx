bfb46400386a5fb89ec3d44ec9b6fa1909b3cfb6
Mhd Ali Houria <alihoria123456@gmail.com>
2023-06-14 17:49:35 +0300

    Adding the Delivery and DeliveryTable files

---
+++ pages/delivery.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
+import PaymentForm from "pages-sections/payment/PaymentForm";
+import PaymentSummary from "pages-sections/payment/PaymentSummary";
+import DeliveryTable from "pages-sections/delivery/DeliveryTable";
+import OrderSummerySummery from "pages-sections/order/OrderSummerySummery";
+import CheckoutNavLayout from "components/layouts/CheckoutNavLayout";
+import { useAppContext } from "../src/contexts/AppContext";
+import usePostFetch from "../src/components/fetch/usePostFetch";
+import useGetFetch from "../src/components/fetch/useGetFetch";
+
+const Delivery = ({ banks }) => {
+  const { state, orderData, setOrderData, userToken } = useAppContext();
+  const [orderSummeryResponse, setOrderSummeryResponse] = useState(null);
+  const [carrier, setCarrier] = useState(null)
+  const [couponToken, setCouponToken] = useState(null);
+
+  console.log(carrier)
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const headers = {
+        "X-localization": "ar",
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      };
+      const body = JSON.stringify({
+        coupon_code: couponToken,
+        carrier_id: orderData.carrierId,
+
+        cart_items: state.cart,
+      });
+      const response = await usePostFetch(
+        "https://sinbad-store.com/api/v2/checkout-cart-summary",
+        headers,
+        body
+      );
+      const data = response.data.data;
+      setOrderSummeryResponse(data);
+    };
+    const doCarrierFetch = async () => {
+      const headers = {
+        "X-localization": "ar",
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      };
+      const body = JSON.stringify({
+        location_id: orderData.shippedLocation_id?.toString(),
+        cart_items: state.cart
+        // location_id: orderData.shippedLocation_id,
+        // cart_items: state.cart,
+      });
+      const response = await usePostFetch(
+        "https://sinbad-store.com/api/v2/checkout-get-carriers",
+        headers,
+        body
+      );
+      console.log("function", response, body)
+      const data = response.data.data;
+      setCarrier(data);
+    };
+
+    if( state.cart.length > 0 ) {
+      doCarrierFetch()
+    }
+    doFetch();
+    setOrderData((prevData) => {
+      return {
+        ...prevData,
+        couponCode: couponToken,
+      };
+    });
+  }, [couponToken, state.cart]);
+
+  return (
+    <CheckoutNavLayout>
+      <Grid container flexWrap="wrap-reverse" spacing={3}>
+        <Grid item lg={8} md={8} xs={12}>
+          <DeliveryTable data={carrier} />
+          {/* <PaymentForm banks={banks}/> */}
+        </Grid>
+
+        <Grid item lg={4} md={4} xs={12}>
+          <OrderSummerySummery
+            setCouponToken={setCouponToken}
+            data={orderSummeryResponse}
+          />
+        </Grid>
+      </Grid>
+    </CheckoutNavLayout>
+  );
+};
+
+export const getStaticProps = async (ctx) => {
+  const requestOptions = {
+    method: "GET",
+    headers: { "X-localization": "ar" },
+    redirect: "follow",
+  };
+
+  const response = await useGetFetch(
+    "https://sinbad-store.com/api/v2/get-banks",
+    requestOptions
+  );
+
+  return {
+    props: {
+      banks: response.data.banks,
+    },
+  };
+};
+
+export default Delivery;
---
+++ src/pages-sections/delivery/DeliveryTable.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import Card1 from "components/Card1";
+import {
+  Button,
+  Grid,
+  Paper,
+  Radio,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  useTheme,
+} from "@mui/material";
+import { currency } from "lib";
+import Link from "next/link";
+
+const DeliveryTable = ({ data }) => {
+  const theme = useTheme();
+  const [checked, setChecked] = useState(null);
+  console.log(checked)
+
+  return (
+    <>
+      <Card1>
+        <Paper>
+          {data &&
+          typeof data.carrier_location !== "undefined" &&
+          data.carrier_location.length > 0 ? (
+            <TableContainer>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead style={{ background: theme.palette.primary.main }}>
+                  <TableRow>
+                    <TableCell
+                      style={{
+                        color: theme.palette.primary.contrastText,
+                        fontWeight: "700",
+                      }}
+                      align="center"
+                    ></TableCell>
+                    <TableCell
+                      style={{
+                        color: theme.palette.primary.contrastText,
+                        fontWeight: "700",
+                      }}
+                      align="center"
+                    >
+                      اسم الشركة
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        color: theme.palette.primary.contrastText,
+                        fontWeight: "700",
+                      }}
+                      align="center"
+                    >
+                      التكلفة
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        color: theme.palette.primary.contrastText,
+                        fontWeight: "700",
+                      }}
+                      align="center"
+                    >
+                      التسليم في غضون
+                    </TableCell>
+                    <TableCell
+                      style={{
+                        color: theme.palette.primary.contrastText,
+                        fontWeight: "700",
+                      }}
+                      align="center"
+                    >
+                      مدة تاخير التسليم
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.carrier_location.map((item, idx) => (
+                    <TableRow
+                      key={idx}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell style={{ textAlign: "center" }}>
+                        <Radio
+                          checked={checked === item.id ? true : false}
+                          color="primary"
+                          size="small"
+                          onClick={() => setChecked(item.id)}
+                        />
+                      </TableCell>
+                      <TableCell style={{ textAlign: "center" }}>
+                        {item.carrier}
+                      </TableCell>
+                      <TableCell style={{ textAlign: "center" }}>
+                        {typeof item.fee === "undefined"
+                          ? currency(0)
+                          : currency(item.fee)}
+                      </TableCell>
+                      <TableCell style={{ textAlign: "center" }}>
+                        {item.delivery_within}
+                      </TableCell>
+                      <TableCell style={{ textAlign: "center" }}>
+                        {item.delivery_delay}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          ) : (
+            <div>
+              Something Went Wrong <br /> You Don't Have Access to the Table
+            </div>
+          )}
+        </Paper>
+      </Card1>
+
+      <Grid container spacing={6} style={{ marginTop: "0rem" }}>
+        <Grid item sm={6} xs={12}>
+          <Link href="/checkout" passHref>
+            <Button variant="outlined" color="primary" type="button" fullWidth>
+              Back to Details
+            </Button>
+          </Link>
+        </Grid>
+
+        <Grid item sm={6} xs={12}>
+          <Link href="/orderSummery" passHref>
+            <Button variant="contained" color="primary" type="submit" fullWidth>
+              Proceed
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default DeliveryTable;
