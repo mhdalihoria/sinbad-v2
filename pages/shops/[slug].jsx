35d32a57181090d12602c56f7c6b6b17c1f89f58
mhdalihoria <alihoria123456@gmail.com>
2023-08-06 21:05:49 +0300

    Adding Filteration Logic

--- pages/shops/[slug].jsx
+++ pages/shops/[slug].jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useGetFetch from "../../src/components/fetch/useGetFetch";
 import ProductCard1 from "../../src/components/product-cards/ProductCard1";
 import { Grid, styled } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import Loader from "../../src/components/loader-spinner/Loader";
+import { useAppContext } from "../../src/contexts/AppContext";
+import usePostFetch from "../../src/components/fetch/usePostFetch";
 
 const IntroContainer = styled("div")({
   width: "80%",
@@ -36,11 +38,26 @@ const CategoryContainer = styled("div")({
   "& ul li": {
     margin: "1em 0",
     transition: "color .7s ease",
+    padding: "5px 10px",
+    cursor: "pointer",
+  },
+
+  "& ul .categoryItem": {
+    margin: "1em 0",
+    background: "#e0e0de",
+    color: "red",
+    borderRadius: "5px",
+    transition: "color .7s ease, background .7s ease",
     cursor: "pointer",
   },
 
   "& ul li:hover": {
     color: "red",
+  },
+
+  "& ul .categoryItem:hover": {
+    color: "black",
+    background: "#c3c3c3",
   },
 });
 
@@ -50,6 +67,22 @@ const CardsContainer = styled("div")({
 });
 
 const ShopDetails = ({ id, shopData }) => {
+  const {
+    products,
+    profile_setting: profileSetting,
+    contacts,
+    featuredProducts,
+    categories,
+    offer,
+    mazad,
+  } = shopData;
+  const { state, orderData, setOrderData, userToken } = useAppContext();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedCategoryProducts, setSelectedCategoryProducts] =
+    useState(null);
+  const [loading, setLoading] = useState(false);
+  console.log(loading);
+
   if (typeof shopData === "undefined") {
     return (
       <div
@@ -66,16 +99,31 @@ const ShopDetails = ({ id, shopData }) => {
     );
   }
 
-  const {
-    products,
-    profile_setting: profileSetting,
-    contacts,
-    featuredProducts,
-    categories,
-    offer,
-    mazad,
-  } = shopData;
-  console.log(shopData);
+  useEffect(() => {
+    const doFetch = async () => {
+      const headers = {
+        "X-localization": "ar",
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      };
+      const body = JSON.stringify({
+        category: [selectedCategory],
+        shop: [id],
+      });
+
+      const response = await usePostFetch(
+        "https://sinbad-store.com/api/v2/filter-products",
+        headers,
+        body
+      );
+      const data = response.data.data.data.products;
+
+      setSelectedCategoryProducts(data);
+      setLoading(false);
+    };
+    doFetch();
+  }, [selectedCategory]);
+
   return (
     <div>
       <IntroContainer>
@@ -130,48 +178,94 @@ const ShopDetails = ({ id, shopData }) => {
             sm={categories.length > 0 ? 9 : 12}
             md={categories.length > 0 ? 9 : 12}
           >
-            {typeof products !== "undefined" && (
-              <Grid container spacing={2}>
-                {products.map((product) => (
-                  <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    lg={categories.length > 0 ? 4 : 3}
-                    key={product.id}
-                  >
-                    <ProductCard1
-                      id={product.id}
-                      slug={product.id}
-                      title={product.product_name}
-                      price={product.product_price}
-                      rating={product.rating}
-                      imgUrl={`${product.thumb}`}
-                      salePrice={product.sale_price}
-                      description={product.product_description?.replace(
-                        /(<([^>]+)>)/gi,
-                        ""
-                      )}
-                      categoryName={product.category_name}
-                      isNew={product.is_new}
-                      isExternal={product.is_external}
-                      shopName={product.shop_name}
-                      hoverEffect
-                      // isFavorited={
-                      //   favItemsLocalStorage.length > 0 &&
-                      //   favItemsLocalStorage.find(
-                      //     (favItem) => favItem.id === item.id
-                      //   ) ?
-                      //   true : false
-                      // }
-                      offer={offer}
-                      isFuture={product.is_future}
-                    />
-                  </Grid>
-                ))}
-              </Grid>
-            )}
+            {typeof products !== "undefined" &&
+              (selectedCategoryProducts &&
+              typeof selectedCategoryProducts !== "undefined" &&
+              selectedCategoryProducts.length > 0 ? (
+                <Grid container spacing={2}>
+                  {selectedCategoryProducts.map((product) => (
+                    <Grid
+                      item
+                      xs={12}
+                      sm={6}
+                      md={4}
+                      lg={categories.length > 0 ? 4 : 3}
+                      key={product.id}
+                      style={loading ? { opacity: "0.5" } : { opacity: "1" }}
+                    >
+                      <ProductCard1
+                        id={product.id}
+                        slug={product.id}
+                        title={product.product_name}
+                        price={product.product_price}
+                        rating={product.rating}
+                        imgUrl={`${product.thumb}`}
+                        salePrice={product.sale_price}
+                        description={product.product_description?.replace(
+                          /(<([^>]+)>)/gi,
+                          ""
+                        )}
+                        categoryName={product.category_name}
+                        isNew={product.is_new}
+                        isExternal={product.is_external}
+                        shopName={product.shop_name}
+                        hoverEffect
+                        // isFavorited={
+                        //   favItemsLocalStorage.length > 0 &&
+                        //   favItemsLocalStorage.find(
+                        //     (favItem) => favItem.id === item.id
+                        //   ) ?
+                        //   true : false
+                        // }
+                        offer={offer}
+                        isFuture={product.is_future}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+              ) : (
+                <Grid container spacing={2}>
+                  {products.map((product) => (
+                    <Grid
+                      item
+                      xs={12}
+                      sm={6}
+                      md={4}
+                      lg={categories.length > 0 ? 4 : 3}
+                      key={product.id}
+                      style={loading ? { opacity: "0.5" } : { opacity: "1" }}
+                    >
+                      <ProductCard1
+                        id={product.id}
+                        slug={product.id}
+                        title={product.product_name}
+                        price={product.product_price}
+                        rating={product.rating}
+                        imgUrl={`${product.thumb}`}
+                        salePrice={product.sale_price}
+                        description={product.product_description?.replace(
+                          /(<([^>]+)>)/gi,
+                          ""
+                        )}
+                        categoryName={product.category_name}
+                        isNew={product.is_new}
+                        isExternal={product.is_external}
+                        shopName={product.shop_name}
+                        hoverEffect
+                        // isFavorited={
+                        //   favItemsLocalStorage.length > 0 &&
+                        //   favItemsLocalStorage.find(
+                        //     (favItem) => favItem.id === item.id
+                        //   ) ?
+                        //   true : false
+                        // }
+                        offer={offer}
+                        isFuture={product.is_future}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+              ))}
           </Grid>
           {categories.length > 0 && (
             <Grid item xs={12} sm={3} md={3}>
@@ -179,9 +273,22 @@ const ShopDetails = ({ id, shopData }) => {
                 <h1 className="categoryTitle">تصنيفات المتاجر</h1>
                 <ul>
                   {categories.map((category) => (
-                    <Link href={category.category_url} className="categoryLink" key={category.category_name}>
-                      <li>{category.category_name}</li>
-                    </Link>
+                    <li
+                      key={category.category_name}
+                      onClick={() => {
+                        setSelectedCategory(
+                          category.category_url.split("/")[5]
+                        );
+                        setLoading(true);
+                      }}
+                      className={
+                        selectedCategory === category.category_url.split("/")[5]
+                          ? "categoryItem"
+                          : ""
+                      }
+                    >
+                      {category.category_name}
+                    </li>
                   ))}
                 </ul>
               </CategoryContainer>
