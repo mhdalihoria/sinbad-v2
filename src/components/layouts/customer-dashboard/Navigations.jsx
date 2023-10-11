fc396695b3ad8cd3079848879e4304d7f5635042
mhdalihoria <alihoria123456@gmail.com>
2023-10-11 20:27:29 +0300

    Changing Lists of Nav Items for Dashboard

--- src/components/layouts/customer-dashboard/Navigations.jsx
+++ src/components/layouts/customer-dashboard/Navigations.jsx
@@ -3,29 +3,24 @@ import { useRouter } from "next/router";
 import { Card, styled, Typography } from "@mui/material";
 import { CreditCard, FavoriteBorder, Person, Place } from "@mui/icons-material";
 import ShoppingBagOutlined from "@mui/icons-material/ShoppingBagOutlined";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import CategoryIcon from "@mui/icons-material/Category";
 import { FlexBox } from "components/flex-box";
-import CustomerService from "components/icons/CustomerService";
 import NavLink from "components/nav-link/NavLink";
 
 // custom styled components
-const MainContainer = styled(Card)(({
-  theme
-}) => ({
+const MainContainer = styled(Card)(({ theme }) => ({
+  paddingTop: "1.5rem",
   paddingBottom: "1.5rem",
   [theme.breakpoints.down("md")]: {
     boxShadow: "none",
     overflowY: "auto",
-    height: "calc(100vh - 64px)"
-  }
+    height: "calc(100vh - 64px)",
+  },
 }));
-const StyledNavLink = styled(({
-  children,
-  isCurrentPath,
-  ...rest
-}) => <NavLink {...rest}>{children}</NavLink>)(({
-  theme,
-  isCurrentPath
-}) => ({
+const StyledNavLink = styled(({ children, isCurrentPath, ...rest }) => (
+  <NavLink {...rest}>{children}</NavLink>
+))(({ theme, isCurrentPath }) => ({
   display: "flex",
   alignItems: "center",
   borderLeft: "4px solid",
@@ -35,71 +30,81 @@ const StyledNavLink = styled(({
   justifyContent: "space-between",
   borderColor: isCurrentPath ? theme.palette.primary.main : "transparent",
   "& .nav-icon": {
-    color: isCurrentPath ? theme.palette.primary.main : theme.palette.grey[600]
+    color: isCurrentPath ? theme.palette.primary.main : theme.palette.grey[600],
   },
   "&:hover": {
     borderColor: theme.palette.primary.main,
     "& .nav-icon": {
-      color: theme.palette.primary.main
-    }
-  }
+      color: theme.palette.primary.main,
+    },
+  },
 }));
 const Navigations = () => {
-  const {
-    pathname
-  } = useRouter();
-  return <MainContainer>
-      {linkList.map(item => <Fragment key={item.title}>
-          <Typography p="26px 30px 1rem" color="grey.600" fontSize="12px">
+  const { pathname } = useRouter();
+  return (
+    <MainContainer>
+      {linkList.map((item) => (
+        <Fragment key={item.title}>
+          {/* <Typography p="26px 30px 1rem" color="grey.600" fontSize="12px">
             {item.title}
-          </Typography>
+          </Typography> */}
 
-          {item.list.map(item => <StyledNavLink href={item.href} key={item.title} isCurrentPath={pathname.includes(item.href)}>
+          {item.list.map((item) => (
+            <StyledNavLink
+              href={item.href}
+              key={item.title}
+              isCurrentPath={pathname.includes(item.href)}
+            >
               <FlexBox alignItems="center" gap={1}>
-                <item.icon color="inherit" fontSize="small" className="nav-icon" />
+                <item.icon
+                  color="inherit"
+                  fontSize="small"
+                  className="nav-icon"
+                />
                 <span>{item.title}</span>
               </FlexBox>
-
-              <span>{item.count}</span>
-            </StyledNavLink>)}
-        </Fragment>)}
-    </MainContainer>;
+            </StyledNavLink>
+          ))}
+        </Fragment>
+      ))}
+    </MainContainer>
+  );
 };
-const linkList = [{
-  title: "DASHBOARD",
-  list: [{
-    href: "/orders",
-    title: "Orders",
-    icon: ShoppingBagOutlined,
-    count: 5
-  }, {
-    href: "/wish-list",
-    title: "Wishlist",
-    icon: FavoriteBorder,
-    count: 19
-  }, {
-    href: "/support-tickets",
-    title: "Support Tickets",
-    icon: CustomerService,
-    count: 1
-  }]
-}, {
-  title: "ACCOUNT SETTINGS",
-  list: [{
-    href: "/profile",
-    title: "Profile Info",
-    icon: Person,
-    count: 3
-  }, {
-    href: "/address",
-    title: "Addresses",
-    icon: Place,
-    count: 16
-  }, {
-    href: "/payment-methods",
-    title: "Payment Methods",
-    icon: CreditCard,
-    count: 4
-  }]
-}];
+const linkList = [
+  {
+    title: "DASHBOARD",
+    list: [
+      {
+        href: "/my-account",
+        title: "Profile Info",
+        icon: Person,
+      },
+      {
+        href: "/orders",
+        title: "Orders",
+        icon: ShoppingBagOutlined,
+      },
+      {
+        href: "/something",
+        title: "Favorite Categories",
+        icon: FavoriteBorder,
+      },
+      {
+        href: "/something",
+        title: "Categories",
+        icon: CategoryIcon,
+      },
+      {
+        href: "/something",
+        title: "Payments",
+        icon: CreditCard,
+      },
+      {
+        href: "/something",
+        title: "Bank Payments",
+        icon: AccountBalanceIcon,
+      },
+    ],
+  },
+];
 export default Navigations;
