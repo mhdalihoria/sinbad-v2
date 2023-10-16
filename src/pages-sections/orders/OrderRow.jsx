1f7429d3c2784d05c03ef305117a39a70c0ccf1a
mhdalihoria <alihoria123456@gmail.com>
2023-10-16 13:07:56 +0300

    Changing file code to fit the data structure from the payment end point

--- src/pages-sections/orders/OrderRow.jsx
+++ src/pages-sections/orders/OrderRow.jsx
@@ -7,67 +7,83 @@ import { H5 } from "components/Typography";
 import { currency } from "lib";
 // =================================================
 
-const OrderRow = ({
-  order
-}) => {
-  const getColor = status => {
+const OrderRow = ({ order }) => {
+  const getColor = (status) => {
     switch (status) {
-      case "Pending":
-        return "secondary";
-      case "Processing":
-        return "secondary";
-      case "Delivered":
+      // case "Pending":
+      //   return "secondary";
+      // case "Processing":
+      //   return "secondary";
+      case "Credit":
         return "success";
-      case "Cancelled":
+      case "Debit":
         return "error";
       default:
         return "";
     }
   };
-  return <Link href={`/orders/${order.id}`} passHref>
-      <a>
-        <TableRow sx={{
-        my: "1rem",
-        padding: "6px 18px"
-      }}>
-          <H5 m={0.75} textAlign="left">
-            {order.id.split("-")[0]}
-          </H5>
+  return (
+    <TableRow
+    sx={{
+      my: "1rem",
+      padding: "6px 18px",
+    }}
+  >
+    <H5 m={0} textAlign="left">
+      {currency(order.amount)}
+    </H5>
+    <Typography className="pre" m={0.75} textAlign="left">
+      {format(new Date(order.payment_date), "MMM dd, yyyy")}
+    </Typography>
 
-          <Box m={0.75}>
-            <Chip size="small" label={order.status} sx={{
-            p: "0.25rem 0.5rem",
-            fontSize: 12,
-            color: !!getColor(order.status) ? `${getColor(order.status)}.900` : "inherit",
-            backgroundColor: !!getColor(order.status) ? `${getColor(order.status)}.100` : "none"
-          }} />
-          </Box>
+    <Box m={0.75}>
+      <Chip
+        size="small"
+        label={order.debit}
+        sx={{
+          p: "0.25rem 0.5rem",
+          fontSize: 12,
+          color: !!getColor(order.debit)
+            ? `${getColor(order.debit)}.900`
+            : "inherit",
+          backgroundColor: !!getColor(order.debit)
+            ? `${getColor(order.debit)}.100`
+            : "none",
+        }}
+      />
+    </Box>
 
-          <Typography className="pre" m={0.75} textAlign="left">
-            {format(new Date(order.createdAt), "MMM dd, yyyy")}
-          </Typography>
+    <Typography m={0.75} textAlign="left">
+      {order.note}
+    </Typography>
 
-          <Typography m={0.75} textAlign="left">
-            {currency(order.totalPrice)}
-          </Typography>
+    <Typography m={0.75} textAlign="left">
+      {currency(order.archive)}
+    </Typography>
 
-          <Typography color="grey.600" textAlign="center" sx={{
-          flex: "0 0 0 !important",
-          display: {
-            xs: "none",
-            md: "block"
-          }
-        }}>
-            <IconButton>
-              <East fontSize="small" color="inherit" sx={{
-              transform: ({
-                direction
-              }) => `rotate(${direction === "rtl" ? "180deg" : "0deg"})`
-            }} />
-            </IconButton>
-          </Typography>
-        </TableRow>
-      </a>
-    </Link>;
+    {/* <Typography
+      color="grey.600"
+      textAlign="center"
+      sx={{
+        flex: "0 0 0 !important",
+        display: {
+          xs: "none",
+          md: "block",
+        },
+      }}
+    >
+      <IconButton>
+        <East
+          fontSize="small"
+          color="inherit"
+          sx={{
+            transform: ({ direction }) =>
+              `rotate(${direction === "rtl" ? "180deg" : "0deg"})`,
+          }}
+        />
+      </IconButton>
+    </Typography> */}
+  </TableRow>
+  );
 };
 export default OrderRow;
