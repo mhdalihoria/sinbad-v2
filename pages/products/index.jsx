5ee4085b049c2d4ad8fa654a16c4b51e9e94872c
mhdalihoria <alihoria123456@gmail.com>
2023-12-05 06:33:22 +0200

    Using "NoProductAlert" Module (newly exported)

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -8,8 +8,8 @@ import { useAppContext } from "../../src/contexts/AppContext";
 import Loader from "../../src/components/loader-spinner/Loader";
 import PageLoader from "../../src/components/loader-spinner/PageLoader";
 import CardLoader from "../../src/components/loader-spinner/CardLoader";
+import NoProductsAlert from "../../src/components/NoProductsAlert";
 import { useRouter } from "next/router";
-import Logo from "../../public/assets/images/header/logo.jpg";
 
 const FilterBlock = styled("div")(({ theme }) => ({
   paddingRight: "10px",
@@ -487,34 +487,6 @@ const Products = ({}) => {
         </div>
       </div>
     </>
-  );
-};
-
-const NoProductsAlert = () => {
-  return (
-    <Card
-      sx={{
-        width: "100%",
-        minHeight: "300px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        gap: "1rem",
-        fontSize: "1.25rem",
-        marginTop: "1rem",
-        padding: "3rem",
-      }}
-    >
-      <Image src={Logo} width={160} height={110} />
-      <div style={{ textAlign: "center" }}>
-        <p style={{ marginBottom: 0 }}>Oops, No Products Here</p>
-
-        <p style={{ fontSize: "1rem", marginTop: 0 }}>
-          Try different search words or filters
-        </p>
-      </div>
-    </Card>
   );
 };
 
